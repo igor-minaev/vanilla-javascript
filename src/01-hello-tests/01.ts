70375cf4da1cdd62c4e03553db442b6309b14798
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-01 21:50:52 +0300

    first test

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,8 +1,9 @@
 export function sum(a: number, b: number) {
     return a + b
 }
+
 export function mult(a: number, b: number) {
-    return a * b
+    return a + b
 }
 
 export function splitIntoWords(sentense: string) {
