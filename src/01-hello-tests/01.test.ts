6ae6823dfad9ce2d08aa82f791d4e301f2d5fb92
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-01 23:21:36 +0300

    refactor test

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -1,13 +1,20 @@
 import {mult, splitIntoWords, sum} from "./01";
 
+let a: number;
+let b: number;
+let c: number;
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
+
 test('sum should be correct', () => {
-    const a = 1;
-    const b = 2;
-    const c = 3;
+
 
     //action
-    const result1 =sum(a,b);
-    const result2 =sum(b,c);
+    const result1 = sum(a, b);
+    const result2 = sum(b, c);
 
     //expect result
     expect(result1).toBe(3);
@@ -15,25 +22,23 @@ test('sum should be correct', () => {
 })
 
 test('multiply should be correct', () => {
-    const a = 1;
-    const b = 2;
-    const c = 3;
+    ;
 
     //action
-    const result1 =mult(a,b);
-    const result2 =mult(b,c);
+    const result1 = mult(a, b);
+    const result2 = mult(b, c);
 
     //expect result
     expect(result1).toBe(2);
     expect(result2).toBe(6);
 })
 
-test('splitting into words should be correct',()=>{
-    const sent1='Hello my friend!'
-    const sent2='JS - the best  programming language'
+test('splitting into words should be correct', () => {
+    const sent1 = 'Hello my friend!'
+    const sent2 = 'JS - the best  programming language'
 
-    const result1= splitIntoWords(sent1)
-    const result2= splitIntoWords(sent2)
+    const result1 = splitIntoWords(sent1)
+    const result2 = splitIntoWords(sent2)
 
     expect(result1.length).toBe(3)
     expect(result1[0]).toBe('hello')
