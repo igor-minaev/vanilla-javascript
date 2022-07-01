36c8e94c85926b8e4bdf34ff7b380f92acfa43d6
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-01 20:57:46 +0300

    first commit

---
+++ src/01/01.ts
@@ -0,0 +1,3 @@
+console.log(1111)
+
+export const value01_01 = '01_01';
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {value01_01} from "./01/01";
+
+
+
+let a = value01_01
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
