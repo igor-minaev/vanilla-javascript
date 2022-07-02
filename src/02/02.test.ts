e3fe0531e473c3c8d71122fcad7bd6efcf26af5d
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-02 09:45:50 +0300

    done test

--- src/02/02.test.ts
+++ src/02/02.test.ts
@@ -5,7 +5,19 @@ let city: CityType;
 beforeEach(() => {
     city = {
         title: 'New York',
-        houses: [],
+        houses: [
+            {
+                buildedAt: 2012, repaired: false,
+                address: {number: 100, street: {title: 'White street'}}
+            },
+            {
+                buildedAt: 2008, repaired: false,
+                address: {number: 100, street: {title: 'Happy street'}}
+            },
+            {
+                buildedAt: 2020, repaired: false,
+                address: {number: 101, street: {title: 'White street'}}
+            }],
         governmentBuildings: [],
         citizensNumber: 1000000
     }
@@ -26,7 +38,7 @@ test('test city should contains 3 houses', () => {
     expect(city.houses[1].address.number).toBe(100)
     expect(city.houses[1].address.street.title).toBe('Happy street')
 
-    expect(city.houses[2].buildedAt).toBe(2028)
+    expect(city.houses[2].buildedAt).toBe(2020)
     expect(city.houses[2].repaired).toBe(false)
     expect(city.houses[2].address.number).toBe(101)
     expect(city.houses[2].address.street.title).toBe('White street')
--- src/02/02_02.ts
+++ src/02/02_02.ts
@@ -1,6 +1,21 @@
+export type StreetType = {
+    title: string
+}
+
+export type AdressType = {
+    number: number
+    street: StreetType
+}
+
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AdressType
+}
+
 export type CityType = {
     title: string
-    houses: Array<string>
+    houses: Array<HouseType>
     governmentBuildings: Array<string>
     citizensNumber: number
 }
