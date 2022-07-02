431565c7739017e4810a66bfc19b8b44998d4dac
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-02 09:32:22 +0300

    add test for object

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,4 +1,29 @@
-const student = {
+type CityType={
+    title:string
+    countryTitle:string
+}
+
+type AddressType={
+    streetTitle:string
+    city:CityType
+}
+
+type TechType={
+    id:number
+    title:string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address:AddressType
+    technologies:Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
     name: 'Dimych',
     age: 32,
     isActive: false,
@@ -9,7 +34,7 @@ const student = {
             countryTitle: 'Belarus'
         }
     },
-    technologies:[
+    technologies: [
         {
             id: 1,
             title: 'HTML'
