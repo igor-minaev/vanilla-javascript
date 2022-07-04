281b8eb4749af4994b397acdda99d5afa9f1d7b4
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-04 21:51:35 +0300

    add third test

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -20,3 +20,18 @@ test('should take courses chipper 160', () => {
     expect(cheapCourses[0].title).toBe('CSS')
     expect(cheapCourses[1].title).toBe('REACT')
 })
+
+test('get only completed tasks', () => {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Salt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const completedTasks=tasks.filter(task=>task.isDone)
+
+    expect(completedTasks.length).toBe(2)
+    expect(completedTasks[0].id).toBe(2)
+    expect(completedTasks[1].id).toBe(4)
+})
