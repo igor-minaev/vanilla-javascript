26f2a54542b5d8565767a7946f8453f4d798bdf6
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-06 13:56:58 +0300

    add onClick and onBlur

--- src/06/06-callback.tsx
+++ src/06/06-callback.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import {logDOM} from "@testing-library/react";
 
-export const User=()=>{
-    const deletUser=()=>{
+export const User = () => {
+    const deletUser = () => {
         alert('user was deleted')
     }
-    const saveUser=()=>{
+    const saveUser = () => {
         alert('user was saved')
     }
-    return <div>Dimych
+    const onNameChanged = () => {
+        console.log('name changed')
+    }
+    const focusLostHandler = () => {
+        console.log('focus lost')
+    }
+    return <div><textarea
+        onChange={onNameChanged}
+        onBlur={focusLostHandler}
+    >Dimych</textarea>
+        <input/>
         <div onClick={deletUser}>delete</div>
         <div onClick={saveUser}>save</div>
     </div>
