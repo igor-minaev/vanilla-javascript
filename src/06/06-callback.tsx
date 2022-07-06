a9fa18883d9f8c3c5c5d1c17c03a3a0cded47e85
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-06 14:22:29 +0300

    refactor

--- src/06/06-callback.tsx
+++ src/06/06-callback.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import {logDOM} from "@testing-library/react";
+import React, {ChangeEvent, MouseEvent} from "react";
+
 
 export const User = () => {
-    const deletUser = () => {
-        alert('user was deleted')
+    const search = (e: MouseEvent<HTMLButtonElement>) => {
+        alert(e.currentTarget.name
+        )
     }
-    const saveUser = () => {
-        alert('user was saved')
+    // const saveUser = () => {
+    //     alert('user was saved')
+    // }
+    const onAgeChanged = (e:ChangeEvent<HTMLInputElement>) => {
+
+        console.log('age changed: ' + e.currentTarget.value)
     }
+
     const onNameChanged = () => {
         console.log('name changed')
     }
+
     const focusLostHandler = () => {
         console.log('focus lost')
     }
@@ -18,8 +25,8 @@ export const User = () => {
         onChange={onNameChanged}
         onBlur={focusLostHandler}
     >Dimych</textarea>
-        <input/>
-        <div onClick={deletUser}>delete</div>
-        <div onClick={saveUser}>save</div>
+        <input onChange={onAgeChanged} type='number'/>
+        <button name='delete' onClick={search}>x</button>
+        {/*<button name='save' onClick={deletUser}>x</button>*/}
     </div>
 }
