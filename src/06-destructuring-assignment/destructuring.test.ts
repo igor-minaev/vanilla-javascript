5c3efa4978977c9bc5ade847a99ceba92f954434
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-06 19:09:08 +0300

    refactor tests

--- src/06-destructuring-assignment/destructuring.test.ts
+++ src/06-destructuring-assignment/destructuring.test.ts
@@ -6,7 +6,7 @@ beforeEach(() => {
     props = {
         name: 'Dimych',
         age: 32,
-        lessons: [{title: '1'}, {title: '2'}],
+        lessons: [{title: '1'}, {title: '2'}, {title: '3'}],
         address: {
             street: {
                 title: 'Nezavisimosti street'
@@ -26,10 +26,10 @@ test('', () => {
     const l = props.lessons
 
     expect(age).toBe(32)
-    expect(lessons.length).toBe(2)
+    expect(lessons.length).toBe(3)
 
     expect(a).toBe(32)
-    expect(l.length).toBe(2)
+    expect(l.length).toBe(3)
     expect(title).toBe('Nezavisimosti street')
 
 })
@@ -38,11 +38,15 @@ test('', () => {
     const l1 = props.lessons[0]
     const l2 = props.lessons[1]
 
-    const [ls1, ls2] = props.lessons
+    const [ls1,...restLessons] = props.lessons
 
     expect(l1.title).toBe('1')
     expect(l2.title).toBe('2')
 
-    expect(ls1.title).toBe('1')
-    expect(ls2.title).toBe('2')
+    // expect(ls1.title).toBe('1')
+    // expect(ls2.title).toBe('2')
+    // expect(ls3.title).toBe('3')
+    expect(restLessons.length).toBe(2)
+    expect(restLessons[0].title).toBe('2')
+
 })
