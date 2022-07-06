1d9731089f126203e9714c7ab9730f985f877846
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-06 16:09:52 +0300

    refactor tests

---
+++ src/06-destructuring-assignment/destructuring.test.ts
@@ -0,0 +1,47 @@
+type ManType = {
+    name: string
+    age: number
+    lessons: Array<{ title: string }>
+    address: {
+        street: {
+            title: string
+        }
+    }
+}
+
+let props: ManType;
+beforeEach(() => {
+    props = {
+        name: 'Dimych',
+        age: 32,
+        lessons: [{title: '1'}, {title: '2'}],
+        address: {
+            street: {
+                title: 'Nezavisimosti street'
+            }
+        }
+    }
+})
+
+test('', () => {
+    //const age = props.age;
+    //const lessons = props.lessons
+    var {age, lessons} = props
+    //const title = props.address.street.title
+    const {title} = props.address.street
+
+    const a = props.age;
+    const l = props.lessons
+
+    expect(age).toBe(32)
+    expect(lessons.length).toBe(2)
+
+    expect(a).toBe(32)
+    expect(l.length).toBe(2)
+    expect(title).toBe('Nezavisimosti street')
+
+})
+
+test('', () => {
+
+})
