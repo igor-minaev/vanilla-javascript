1d1f6e40fb916a5507c35fd6b4c35c93128e02e4
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-08 18:08:28 +0300

    add test

---
+++ src/08/08_01.test.ts
@@ -0,0 +1,21 @@
+type UsersType = {
+    [key: string]: { id: number, name: string }
+}
+
+export let users: UsersType
+
+beforeEach(() => {
+    users = {
+        '101': {id: 101, name: 'Dimych'},
+        '3232424': {id: 3232424, name: 'Natasha'},
+        '12121': {id: 12121, name: 'Valera'},
+        '1': {id: 1, name: 'Katya'}
+    }
+})
+
+test('should update corresponding user from obj', () => {
+    users['1'].name = 'Ekaterina'
+    expect(users['1'].name).toBe('Ekaterina')
+    expect(users['1']).toEqual({id: 1, name: 'Ekaterina'})
+})
+
--- src/08/08_01.ts
+++ src/08/08_01.ts
@@ -1,19 +1,12 @@
+import {users} from "./08_01.test";
+
 export const usersObj = {
     '0': 'Dimych',
     '1': 'Natasha',
     '2': 'Valera',
     '3': 'Katya'
 }
-type UsersType = {
-    [key: string]: { id: number, name: string }
-}
 
-export const users: UsersType = {
-    '101': {id: 101, name: 'Dimych'},
-    '3232424': {id: 3232424, name: 'Natasha'},
-    '12121': {id: 12121, name: 'Valera'},
-    '1': {id: 1, name: 'Katya'}
-}
 
 //users[1]
 
