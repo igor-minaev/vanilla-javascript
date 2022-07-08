ad3720c041040f8863275a7d949e26b025299e76
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-08 17:39:19 +0300

    add objects

---
+++ src/08/08_01.ts
@@ -0,0 +1,20 @@
+export const usersObj = {
+    '0': 'Dimych',
+    '1': 'Natasha',
+    '2': 'Valera',
+    '3': 'Katya'
+}
+
+export const users = {
+    '101': {id: 101, name: 'Dimych'},
+    '3232424': {id: 3232424, name: 'Natasha'},
+    '12121': {id: 12121, name: 'Valera'},
+    '1': {id: 1, name: 'Katya'}
+}
+
+export const userArray = [
+    {id: 101, name: 'Dimych'},
+    'Natasha', {id: 3232424, name: 'Natasha'},
+    {id: 12121, name: 'Valera'},
+    {id: 1, name: 'Katya'}
+]
