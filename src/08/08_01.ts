b425dd098f2a53c79bb5dc729db2cf4f229eaf0d
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-08 17:50:44 +0300

    add new values

--- src/08/08_01.ts
+++ src/08/08_01.ts
@@ -4,17 +4,27 @@ export const usersObj = {
     '2': 'Valera',
     '3': 'Katya'
 }
+type UsersType = {
+    [key: string]: { id: number, name: string }
+}
 
-export const users = {
+export const users:UsersType = {
     '101': {id: 101, name: 'Dimych'},
     '3232424': {id: 3232424, name: 'Natasha'},
     '12121': {id: 12121, name: 'Valera'},
     '1': {id: 1, name: 'Katya'}
 }
 
+//users[1]
+
+var user = {id: 100500, name: 'Igor'}
+users[user.id] = user
+
 export const userArray = [
     {id: 101, name: 'Dimych'},
-    'Natasha', {id: 3232424, name: 'Natasha'},
+    {id: 3232424, name: 'Natasha'},
     {id: 12121, name: 'Valera'},
     {id: 1, name: 'Katya'}
 ]
+
+//userArray.find(u => u.id === 1)
