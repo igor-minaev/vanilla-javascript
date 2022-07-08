c78a5e3f78eac091952f614bc7abe60ec90d89ea
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-08 18:01:28 +0300

    add methods

--- src/08/08_01.ts
+++ src/08/08_01.ts
@@ -8,7 +8,7 @@ type UsersType = {
     [key: string]: { id: number, name: string }
 }
 
-export const users:UsersType = {
+export const users: UsersType = {
     '101': {id: 101, name: 'Dimych'},
     '3232424': {id: 3232424, name: 'Natasha'},
     '12121': {id: 12121, name: 'Valera'},
@@ -17,14 +17,19 @@ export const users:UsersType = {
 
 //users[1]
 
-var user = {id: 100500, name: 'Igor'}
-users[user.id] = user
+var user = {id: 100500, name: 'Igor'}  //поиск
+users[user.id] = user                  //добавление
+delete users[user.id]                  //удаление
+users[user.id].name= 'Vitya'
 
-export const userArray = [
+export const usersArray = [
     {id: 101, name: 'Dimych'},
     {id: 3232424, name: 'Natasha'},
     {id: 12121, name: 'Valera'},
     {id: 1, name: 'Katya'}
 ]
 
-//userArray.find(u => u.id === 1)
+//usersArray.find(u => u.id === 1)
+//usersArray.push(user)
+//var usersCopy = [...usersArray.filter(), user]
+//var users = usersArray.filter(u => u.id !== user.id)
